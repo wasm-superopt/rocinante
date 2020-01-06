5833980ac1ccd9481350bffec4f3a060dc6f9d91
Taegyun Kim <k.taegyun@gmail.com>
2020-01-06 13:32:34 -0500

    Add z3 example back.

---
+++ examples/z3.rs
@@ -0,0 +1,18 @@
+extern crate env_logger;
+extern crate log;
+extern crate z3;
+
+use z3::*;
+
+fn main() {
+    println!("Hello, world; trying z3 example!");
+    let _ = env_logger::try_init();
+    let cfg = Config::new();
+    let ctx = Context::new(&cfg);
+    let x = ast::Int::new_const(&ctx, "x");
+    let y = ast::Int::new_const(&ctx, "y");
+
+    let solver = Solver::new(&ctx);
+    solver.assert(&x.gt(&y));
+    assert_eq!(solver.check(), SatResult::Sat);
+}
