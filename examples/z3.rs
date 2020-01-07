63c69f5fdfeb17a30784e0ee1cffcaf3b79705cf
Taegyun Kim <k.taegyun@gmail.com>
2020-01-06 22:43:56 -0500

    Add datatype building example from z3.rs

--- examples/z3.rs
+++ examples/z3.rs
@@ -2,17 +2,66 @@ extern crate env_logger;
 extern crate log;
 extern crate z3;
 
-use z3::*;
+use z3::{
+    ast::{Ast, Datatype, Int},
+    Config, Context, DatatypeBuilder, SatResult, Solver, Sort,
+};
 
-fn main() {
-    println!("Hello, world; trying z3 example!");
+fn datatype() {
+    let cfg = Config::new();
+    let ctx = Context::new(&cfg);
+    let solver = Solver::new(&ctx);
+    // Like Rust's Option<int> type
+    let option_int = DatatypeBuilder::new(&ctx)
+        .variant("None", &[])
+        .variant("Some", &[("value", &Sort::int(&ctx))])
+        .finish("OptionInt");
+
+    // Assert x.is_none()
+    let x = Datatype::new_const(&ctx, "x", &option_int.sort);
+    solver.assert(
+        &option_int.variants[0]
+            .tester
+            .apply(&[&x.into()])
+            .as_bool()
+            .unwrap(),
+    );
+
+    // Assert y == Some(3)
+    let y = Datatype::new_const(&ctx, "y", &option_int.sort);
+    let value = option_int.variants[1]
+        .constructor
+        .apply(&[&Int::from_i64(&ctx, 3).into()]);
+    solver.assert(&y._eq(&value.as_datatype().unwrap()));
+
+    assert_eq!(solver.check(), SatResult::Sat);
+    let model = solver.get_model();
+
+    // Get the value out of Some(3)
+    let ast = option_int.variants[1].accessors[0].apply(&[&y.into()]);
+    assert_eq!(
+        3,
+        model
+            .eval(&ast.as_int().unwrap())
+            .unwrap()
+            .as_i64()
+            .unwrap()
+    );
+}
+
+fn simple() {
     let _ = env_logger::try_init();
     let cfg = Config::new();
     let ctx = Context::new(&cfg);
-    let x = ast::Int::new_const(&ctx, "x");
-    let y = ast::Int::new_const(&ctx, "y");
+    let x = Int::new_const(&ctx, "x");
+    let y = Int::new_const(&ctx, "y");
 
     let solver = Solver::new(&ctx);
     solver.assert(&x.gt(&y));
     assert_eq!(solver.check(), SatResult::Sat);
+}
+
+fn main() {
+    simple();
+    datatype();
 }
