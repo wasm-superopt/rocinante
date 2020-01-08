4a3f05871fedade24c749728ead97d9451d84a3b
Taegyun Kim <k.taegyun@gmail.com>
2020-01-08 14:30:12 -0500

    Add examples for addition and shift.

--- examples/z3.rs
+++ examples/z3.rs
@@ -3,7 +3,7 @@ extern crate log;
 extern crate z3;
 
 use z3::{
-    ast::{Ast, Datatype, Int},
+    ast::{Ast, Datatype, Int, BV},
     Config, Context, DatatypeBuilder, SatResult, Solver, Sort,
 };
 
@@ -61,7 +61,34 @@ fn simple() {
     assert_eq!(solver.check(), SatResult::Sat);
 }
 
+fn shift() {
+    let _ = env_logger::try_init();
+    let cfg = Config::new();
+    let ctx = Context::new(&cfg);
+    let x = BV::new_const(&ctx, "x", 32);
+    let one = BV::from_i64(&ctx, 1, 32);
+    let two = BV::from_i64(&ctx, 2, 32);
+
+    let solver = Solver::new(&ctx);
+    solver.assert(&x.bvshl(&one).bvshl(&one)._eq(&x.bvshl(&two)));
+
+    assert_eq!(solver.check(), SatResult::Sat);
+}
+
+fn add() {
+    let _ = env_logger::try_init();
+    let cfg = Config::new();
+    let ctx = Context::new(&cfg);
+    let x = Int::new_const(&ctx, "x");
+    let solver = Solver::new(&ctx);
+    solver.assert(&x.add(&[&x])._eq(&x.mul(&[&Int::from_i64(&ctx, 2)])));
+
+    assert_eq!(solver.check(), SatResult::Sat);
+}
+
 fn main() {
     simple();
     datatype();
+    shift();
+    add();
 }
