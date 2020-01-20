cfab47b27a2f6b3b821e9110bba6a137875e2a6f
Taegyun Kim <k.taegyun@gmail.com>
2020-01-20 12:36:20 -0500

    Add test case for exec.

--- src/exec.rs
+++ src/exec.rs
@@ -4,47 +4,14 @@ use wasmi::{nan_preserving_float, Error, ModuleInstance, NopExternals, RuntimeVa
 
 const NUM_TEST_CASES: usize = 10;
 
-#[derive(Clone, Debug)]
-pub struct Input(Vec<RuntimeValue>);
+type Input = Vec<RuntimeValue>;
 
-impl Input {
-    pub fn new(elements: Vec<RuntimeValue>) -> Self {
-        Input(elements)
-    }
-    pub fn elements(&self) -> &[RuntimeValue] {
-        &self.0
-    }
-}
+type Output = Result<Option<RuntimeValue>, Error>;
 
-#[derive(Debug)]
-pub struct Output(Result<Option<RuntimeValue>, Error>);
-
-impl Output {
-    pub fn new(result: Result<Option<RuntimeValue>, Error>) -> Self {
-        Output(result)
-    }
-    pub fn result(&self) -> &Result<Option<RuntimeValue>, Error> {
-        &self.0
-    }
-}
-
-#[derive(Debug)]
-pub struct TestCases(Vec<(Input, Output)>);
-
-impl TestCases {
-    pub fn new(elements: Vec<(Input, Output)>) -> Self {
-        TestCases(elements)
-    }
-    pub fn elements(&self) -> &[(Input, Output)] {
-        &self.0
-    }
-    pub fn add_element(&mut self, test_case: (Input, Output)) {
-        self.0.push(test_case)
-    }
-}
+type TestCases = Vec<(Input, Output)>;
 
 fn gen_random_input<R: Rng>(rng: &mut R, param_types: &[ValueType]) -> Input {
-    let mut args: Vec<RuntimeValue> = Vec::with_capacity(param_types.len());
+    let mut inputs = Vec::with_capacity(param_types.len());
 
     for param_type in param_types {
         let arg = match param_type {
@@ -57,10 +24,10 @@ fn gen_random_input<R: Rng>(rng: &mut R, param_types: &[ValueType]) -> Input {
                 RuntimeValue::F64(nan_preserving_float::F64::from_float(rng.gen::<f64>()))
             }
         };
-        args.push(arg);
+        inputs.push(arg);
     }
 
-    Input::new(args)
+    inputs
 }
 
 pub fn generate_test_cases<R: Rng>(
@@ -76,14 +43,83 @@ pub fn generate_test_cases<R: Rng>(
         inputs.push(gen_random_input(rng, signature.params()));
     }
 
+    invoke_with_inputs(instance, func_name, &inputs)
+}
+
+pub fn invoke_with_inputs(
+    instance: &ModuleInstance,
+    func_name: &str,
+    inputs: &[Input],
+) -> TestCases {
     let mut test_cases: Vec<(Input, Output)> = Vec::new();
-
     for input in inputs {
-        let output =
-            Output::new(instance.invoke_export(func_name, input.elements(), &mut NopExternals));
-
-        test_cases.push((input, output));
+        let output = instance.invoke_export(func_name, input, &mut NopExternals);
+        test_cases.push((input.clone(), output));
     }
+    test_cases
+}
 
-    TestCases::new(test_cases)
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use wasmi::ImportsBuilder;
+
+    #[test]
+    fn test_invoke() {
+        let wasm_binary: Vec<u8> = wabt::wat2wasm(
+            r#"
+            (module
+                (type $t0 (func (param i32) (result i32)))
+                (func $div (type $t0) (param $p0 i32) (result i32)
+                  i32.const 4
+                  get_local $p0
+                  i32.div_u)
+                (export "div" (func $div)))
+                              "#,
+        )
+        .expect("failed to parse wat");
+
+        // Load wasm binary and prepare it for instantiation.
+        let module = wasmi::Module::from_buffer(&wasm_binary).expect("failed to load wasm");
+
+        // Instantiate a module with empty imports and
+        // assert that there is no `start` function.
+        let instance = ModuleInstance::new(&module, &ImportsBuilder::default())
+            .expect("failed to instantiate wasm module")
+            .assert_no_start();
+
+        let expected_input: Vec<Input> =
+            vec![vec![RuntimeValue::I32(2)], vec![RuntimeValue::I32(0)]];
+
+        let expected_output: Vec<Output> = vec![
+            Result::Ok(Some(RuntimeValue::I32(2))),
+            Result::Err(Error::Trap(wasmi::Trap::new(
+                wasmi::TrapKind::DivisionByZero,
+            ))),
+        ];
+
+        let test_cases = invoke_with_inputs(&instance, "div", &expected_input);
+
+        assert_eq!(expected_input.len(), test_cases.len());
+        for i in 0..expected_input.len() {
+            let expected_input = &expected_input[i];
+            let expected_output = &expected_output[i];
+
+            let (actual_input, actual_output) = &test_cases[i];
+
+            assert_eq!(expected_input, actual_input);
+
+            if expected_output.is_err() {
+                assert_eq!(
+                    expected_output.as_ref().err().unwrap().to_string(),
+                    actual_output.as_ref().err().unwrap().to_string(),
+                );
+            } else {
+                assert_eq!(
+                    expected_output.as_ref().ok().unwrap(),
+                    actual_output.as_ref().ok().unwrap(),
+                );
+            }
+        }
+    }
 }
