0c1893794bca5350ba0be1b15bc64f50216d4fcc
Taegyun Kim <k.taegyun@gmail.com>
2020-01-20 22:05:47 -0500

    Rough draft of synthesize.

--- src/stoke/mod.rs
+++ src/stoke/mod.rs
@@ -13,4 +13,25 @@ impl Optimizer {
     }
 
     pub fn run(&self) {}
+
+    /// Finds a module that has functions equivalent to the functions in the given module.
+    pub fn synthesize(&self) {
+        // for func in module.functions {
+        //   let test_cases = generate_teset_cases(func)
+        //   let candidate = generate_random_func(func.type);
+        //     while (!candidate.validate()) {
+        //       do_transform(candidate)
+        //       if !exec_test_cases(func, test_cases) {
+        //          continue
+        //       }
+        //       match verify(candidate, func) {
+        //          Verified => break,
+        //          CounterExample(inputs) => {
+        //              let exepected_output = func.invoke_with(inputs);
+        //              test_cases.push((inputs, output))
+        //          }
+        //       }
+        //    }
+        // }
+    }
 }
