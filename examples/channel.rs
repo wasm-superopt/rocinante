29677244ca04a974ee749c49e82d9d4575073516
Taegyun Kim <k.taegyun@gmail.com>
2020-02-05 20:15:56 -0500

    Use bus to broadcast messages to terminate.

--- examples/channel.rs
+++ examples/channel.rs
@@ -1,25 +1,51 @@
+extern crate bus;
+extern crate num_cpus;
+
 use rand::prelude::*;
-use std::sync::mpsc::sync_channel;
+use std::sync::mpsc::{sync_channel, TryRecvError};
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let num_workers = 10;
+    let num_workers = num_cpus::get();
 
-    let (sender, receiver) = sync_channel(num_workers);
+    let wait_for_all = false;
+
+    let (res_sender, res_receiver) = sync_channel(num_workers);
+    let mut bus = bus::Bus::new(num_workers);
 
     let mut rng = thread_rng();
     for i in 0..num_workers {
         let second = rng.gen_range(0, 30);
-        let sender_i = sender.clone();
+        let res_sender_i = res_sender.clone();
+        let mut bus_rx_i = bus.add_rx();
         thread::spawn(move || {
             println!("worker {}, sleeping for {} seconds", i, second);
-            thread::sleep(Duration::from_secs(second));
-            sender_i.send(i).unwrap();
+            for _ in 0..second {
+                match bus_rx_i.try_recv() {
+                    Ok(_) | Err(TryRecvError::Disconnected) => {
+                        println!("worker {} received signal to terminate.", i);
+                        break;
+                    }
+                    Err(TryRecvError::Empty) => {
+                        thread::sleep(Duration::from_secs(1));
+                    }
+                }
+            }
+            res_sender_i.send(i).unwrap();
         });
     }
 
     for _ in 0..num_workers {
-        println!("worker {} terminated", receiver.recv().unwrap());
+        println!("worker {} terminated", res_receiver.recv().unwrap());
+        if !wait_for_all {
+            break;
+        }
     }
+
+    // broadcast termination.
+    bus.broadcast(());
+
+    // Wait for workers to terminate
+    thread::sleep(Duration::from_secs(5));
 }
