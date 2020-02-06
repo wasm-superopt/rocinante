6dc6c84a0b954f2ace51d11af2bb0d68db9cce8d
Taegyun Kim <k.taegyun@gmail.com>
2020-02-05 20:50:27 -0500

    Add a commandline flag to control whether wait for all workers.

--- examples/channel.rs
+++ examples/channel.rs
@@ -1,17 +1,32 @@
 extern crate bus;
+extern crate clap;
 extern crate num_cpus;
 
+use clap::{App, Arg};
 use rand::prelude::*;
 use std::sync::mpsc::{sync_channel, TryRecvError};
 use std::thread;
 use std::time::Duration;
 
 fn main() {
+    let matches = App::new(
+        "Programming excercise using multi-producer
+         single consumer channel, and broadcast channel.",
+    )
+    .arg(
+        Arg::with_name("wait_for_all")
+            .help("If set waits for all workers to terminate.")
+            .short("w"),
+    )
+    .get_matches();
+    // Get the number of logical cores available on the machine.
     let num_workers = num_cpus::get();
 
-    let wait_for_all = false;
+    let wait_for_all = matches.is_present("w");
 
+    // A buffered channel to receive computation results from workers.
     let (res_sender, res_receiver) = sync_channel(num_workers);
+    // A broadcast channel to
     let mut bus = bus::Bus::new(num_workers);
 
     let mut rng = thread_rng();
@@ -21,31 +36,33 @@ fn main() {
         let mut bus_rx_i = bus.add_rx();
         thread::spawn(move || {
             println!("worker {}, sleeping for {} seconds", i, second);
-            for _ in 0..second {
+            let mut t = 0;
+            while t < second {
                 match bus_rx_i.try_recv() {
                     Ok(_) | Err(TryRecvError::Disconnected) => {
-                        println!("worker {} received signal to terminate.", i);
                         break;
                     }
                     Err(TryRecvError::Empty) => {
                         thread::sleep(Duration::from_secs(1));
                     }
                 }
+                t += 1;
             }
-            res_sender_i.send(i).unwrap();
+            res_sender_i.send((i, t == second)).unwrap();
         });
     }
 
     for _ in 0..num_workers {
-        println!("worker {} terminated", res_receiver.recv().unwrap());
-        if !wait_for_all {
-            break;
+        let (i, completed) = res_receiver.recv().unwrap();
+        if completed {
+            println!("Worker {} completed its workload.", i);
+        } else {
+            println!("Worker {} terminated by broadcast.", i);
+        }
+        if !wait_for_all && completed {
+            // NOTE(taegyunkim):: Not sure what would happen if this broadcast is called multiple
+            // times.
+            bus.broadcast(());
         }
     }
-
-    // broadcast termination.
-    bus.broadcast(());
-
-    // Wait for workers to terminate
-    thread::sleep(Duration::from_secs(5));
 }
