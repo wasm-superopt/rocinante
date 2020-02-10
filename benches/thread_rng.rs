82c5e829e1e5f5304c3097c94d380a21c1399c50
Taegyun Kim <k.taegyun@gmail.com>
2020-02-09 19:17:46 -0500

    Add comment for initilization.

--- benches/thread_rng.rs
+++ benches/thread_rng.rs
@@ -12,6 +12,7 @@ fn thread_rng_outer<R: Rng + ?Sized>(rng: &mut R) {
 fn bench_thread_rng(c: &mut Criterion) {
     let mut group = c.benchmark_group("ThreadRng");
 
+    // As thread_rng is lazily-initialized make sure that it's initliazed here.
     let mut rng = rand::thread_rng();
     let _ = rng.gen::<i32>();
 
