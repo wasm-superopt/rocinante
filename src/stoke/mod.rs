7b3d04918f7af6fd9b09d0f1276e6777639ea6ea
Taegyun Kim <k.taegyun@gmail.com>
2020-03-09 00:33:34 -0400

    Print only when valid.

--- src/stoke/mod.rs
+++ src/stoke/mod.rs
@@ -264,7 +264,9 @@ impl Superoptimizer {
                             #[cfg(debug_assertions)]
                             println!("accepted...");
                             curr_cost = new_cost;
-                            println!("{}", curr_cost);
+                            if curr_cost < interpreter.score_invalid() {
+                                println!("{}", curr_cost);
+                            }
                         }
                     }
                 }
