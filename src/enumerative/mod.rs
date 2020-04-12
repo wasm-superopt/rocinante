9e17631a11b80785f16545141cc4385089228018
Taegyun Kim <k.taegyun@gmail.com>
2020-04-12 17:01:58 -0400

    Add memoization of seen program states.

--- src/enumerative/mod.rs
+++ src/enumerative/mod.rs
@@ -19,29 +19,60 @@ pub fn search(
     let mut candidates: BinaryHeap<wasm::Candidate> = BinaryHeap::new();
     candidates.push(wasm::Candidate::new(spec.num_instrs()));
 
+    let mut seen_states: Vec<_> = Vec::new();
+    let mut seen_candidates: Vec<wasm::Candidate> = Vec::new();
+
     while !candidates.is_empty() {
         if rx.try_recv().is_ok() {
             println!("Enumerative search timed out.");
             return None;
         }
 
+        let mut seen = false;
         let candidate = candidates.pop().unwrap();
-        if candidate.num_values_on_stack() == return_type_len
-            && interpreter.eval_test_cases(spec.get_binary_with_instrs(candidate.instrs())) == 0
-        {
-            match z3_solver.verify(&candidate.instrs()) {
-                solver::VerifyResult::Verified => {
-                    return Some(candidate);
+        if candidate.num_values_on_stack() == return_type_len {
+            let test_outputs =
+                interpreter.get_test_outputs(spec.get_binary_with_instrs(candidate.instrs()));
+
+            if test_outputs.is_empty() {
+                match z3_solver.verify(&candidate.instrs()) {
+                    solver::VerifyResult::Verified => {
+                        return Some(candidate);
+                    }
+                    solver::VerifyResult::CounterExample(values) => {
+                        interpreter.add_test_case(values);
+                        seen_candidates.push(candidate.clone());
+                        seen_states = seen_candidates
+                            .iter()
+                            .map(|seen_candidate| {
+                                interpreter.get_test_outputs(
+                                    spec.get_binary_with_instrs(seen_candidate.instrs()),
+                                )
+                            })
+                            .collect();
+                    }
                 }
-                solver::VerifyResult::CounterExample(values) => {
-                    interpreter.add_test_case(values);
+            } else {
+                match seen_states.iter().position(|s| *s == test_outputs) {
+                    Some(idx) => {
+                        seen = true;
+                        if candidate.instrs().len() < seen_candidates[idx].instrs().len() {
+                            seen_candidates[idx] = candidate.clone();
+                        }
+                    }
+                    None => {
+                        seen_states.push(test_outputs);
+                        seen_candidates.push(candidate.clone());
+                    }
                 }
             }
         }
 
-        for instr in instr_whitelist.iter() {
-            if let Ok(new_candidate) = candidate.try_append(&instr_whitelist, instr.clone()) {
-                candidates.push(new_candidate);
+        if !seen {
+            for instr in instr_whitelist.iter() {
+                if let Ok(new_candidate) = candidate.try_append(&instr_whitelist, instr.clone()) {
+                    candidates.push(new_candidate);
+                }
             }
         }
     }
