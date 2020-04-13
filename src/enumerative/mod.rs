9f172edf8862499c6fb4bd5af5206431440c0236
Taegyun Kim <k.taegyun@gmail.com>
2020-04-13 00:41:05 -0400

    Add comments.

--- src/enumerative/mod.rs
+++ src/enumerative/mod.rs
@@ -15,10 +15,20 @@ pub fn search(
     let max_length = spec.num_instrs();
 
     // TODO(taegyunkim): Consider using HashMap instead of vectors for better lookup performance.
-    let mut seen_states: Vec<_> = Vec::new();
+    // the ith element in seen_states are obtained from evaluating test cases on ith element in
+    // seen candidates.
     let mut seen_candidates: Vec<Vec<parity_wasm::elements::Instruction>> = Vec::new();
+    let mut seen_states: Vec<_> = Vec::new();
 
+    // Enumerates programs of length i to max_length
     for i in 1..=max_length {
+        // Creates a multi cartesian product of iterators over the whitelisted instructions.
+        // For example, if we're given [1, 2, 3], then there are 9 length 2 candidates as following
+        // [1, 1], [1, 2], [1, 3].
+        // [2, 1], [2, 2], [2, 3],
+        // [3, 1], [3, 2], [3, 3],
+        // Iterators are cheap to copy and each refer to an element in the vector it was created
+        // from.
         let iter = (0..i)
             .map(|_| instr_whitelist.iter())
             .multi_cartesian_product();
@@ -30,8 +40,11 @@ pub fn search(
             }
 
             if let wasm::StackState::Valid = wasm::check_stack_state(&instr_whitelist, &candidate) {
+                // Explicitly copy the instruction list to keep track of them.
                 let instrs: Vec<parity_wasm::elements::Instruction> =
                     candidate.iter().map(|&item| item.clone()).collect();
+                // Get test outputs returns the output values that are different from the spec, so
+                // if this vector is empty, all test cases pass.
                 let test_outputs =
                     interpreter.get_test_outputs(spec.get_binary_with_instrs(&instrs));
                 if test_outputs.is_empty() {
